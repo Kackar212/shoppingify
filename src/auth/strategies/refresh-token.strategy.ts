b71fc81e430ca4c07444786679088c1a8f89d6e1
QoB <kackar212@gmail.com>
2022-12-12 12:35:36 +0100

    Create RefreshTokenStrategy

---
+++ src/auth/strategies/refresh-token.strategy.ts
@@ -0,0 +1,30 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Request } from 'express';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class RefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refresh-token') {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request: Request) => {
+          return request?.cookies?.RefreshToken;
+        },
+      ]),
+      secretOrKey: configService.get('REFRESH_JWT_SECRET'),
+      passReqToCallback: true,
+    });
+  }
+
+  async validate(request: Request, payload: TokenPayload) {
+    const refreshToken = request.cookies?.Refresh;
+
+    return this.userService.getUserIfRefreshTokenMatches(payload.name, refreshToken);
+  }
+}
