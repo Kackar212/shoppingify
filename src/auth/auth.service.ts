a697147f698a0aa688bb29e4a4563b1967f24161
QoB <kackar212@gmail.com>
2022-12-12 07:07:32 +0100

    Create createRefreshToken method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt/dist';
 import { User } from 'src/user/user.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
   ) {}
 
   async getUser(email: string, password: string) {
@@ -31,5 +33,15 @@ export class AuthService {
     const payload = { name: user.name };
 
     return this.jwtService.signAsync(payload);
+  }
+
+  public createRefreshToken({ name }: User) {
+    return this.jwtService.signAsync(
+      { name },
+      {
+        secret: this.configService.get('REFRESH_JWT_SECRET'),
+        expiresIn: this.configService.get('REFRESH_JWT_EXPIRATION_TIME'),
+      },
+    );
   }
 }
