0fbd0b8fb315695ad9ef30e15a9d4238bc7f3b60
QoB <kackar212@gmail.com>
2022-12-12 05:54:28 +0100

    Add login method to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { User } from 'src/common/decorators/user.decorator';
+import { User as UserEntity } from 'src/user/user.entity';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  public async login(@User() user: UserEntity) {
+    return user;
+  }
+}
