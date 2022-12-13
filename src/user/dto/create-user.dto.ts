cbdac37f35b33ccc126f919543d90b693a3fb7b7
QoB <kackar212@gmail.com>
2022-12-13 06:18:16 +0100

    Add validation to user registration

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,18 @@
+import { IsString } from 'class-validator';
+import { IsEmail, IsAlphanumeric, Length, Matches } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @Length(3, 20)
+  @IsAlphanumeric()
+  @Matches(/^[A-Z].*/i)
   name: string;
+
+  @IsEmail()
+  @Length(3, 100)
   email: string;
+
+  @IsString()
+  @Length(8, 30)
   password: string;
 }
