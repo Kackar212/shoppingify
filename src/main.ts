48424ddfab904a8fe256ec0c233f5c9d8243c603
QoB <kackar212@gmail.com>
2022-12-16 10:49:54 +0100

    Add SSL

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+  });
 
   app.use(cookieParser());
   app.useGlobalPipes(
