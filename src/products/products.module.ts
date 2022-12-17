60f6a2a1a289a6c1428b3d465cb8fb24973922fb
QoB <kackar212@gmail.com>
2022-12-17 12:21:12 +0100

    Add CategoriesModule to imports in ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from 'src/categories/categories.module';
 import { Product } from './product.entity';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [TypeOrmModule.forFeature([Product]), CategoriesModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
