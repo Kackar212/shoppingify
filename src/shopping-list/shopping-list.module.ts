fc5e59ac7f4f227cebf1ddc1b74dd6d949da81a8
QoB <kackar212@gmail.com>
2022-12-18 07:02:26 +0100

    Create ShoppingListService

--- src/shopping-list/shopping-list.module.ts
+++ src/shopping-list/shopping-list.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShoppingList } from './shopping-list.entity';
+import { ShoppingListService } from './shopping-list.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ShoppingList])],
+  providers: [ShoppingListService],
 })
 export class ShoppingListModule {}
