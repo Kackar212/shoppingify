c254a867810a7228821a5b0f1ac2b4d3f5a71ced
QoB <kackar212@gmail.com>
2022-12-22 00:06:07 +0100

    Create getActiveList method

---
+++ src/shopping-list/enums/status.enum.ts
@@ -0,0 +1,5 @@
+export enum STATUS {
+  ACTIVE = 'active',
+  COMPLETED = 'completed',
+  CANCELED = 'canceled',
+}
--- src/shopping-list/shopping-list.service.ts
+++ src/shopping-list/shopping-list.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+import { ShoppingListProduct } from './shopping-list-product.entity';
+import { ShoppingList } from './shopping-list.entity';
 
 @Injectable()
-export class ShoppingListService {}
+export class ShoppingListService {
+  constructor(
+    @InjectRepository(ShoppingList)
+    private readonly shoppingListRepository: Repository<ShoppingList>,
+
+    @InjectRepository(ShoppingListProduct)
+    private readonly shoppingListProductRepository: Repository<ShoppingListProduct>,
+  ) {}
+
+  public async getActiveList(user: User) {
+    const activeList = await this.shoppingListRepository.findOne({
+      where: { status: 'active', user },
+      relations: ['products', 'products.product'],
+    });
+
+    if (activeList) {
+      return activeList;
+    }
+
+    return await this.shoppingListRepository.save({
+      status: 'active',
+      products: [],
+      user,
+    });
+  }
+}
