5be27c51a2bba5d2fe20fa3820026a22de004252
QoB <kackar212@gmail.com>
2022-12-22 00:42:26 +0100

    Create addProduct method in ShoppingListController

--- src/shopping-list/shopping-list.controller.ts
+++ src/shopping-list/shopping-list.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { User } from 'src/common/decorators/user.decorator';
+import { AddShoppingListProductDto } from './dto/add-shopping-list-product.dto';
+import { ShoppingListService } from './shopping-list.service';
+import { User as UserEntity } from 'src/user/user.entity';
 
 @Controller('shopping-list')
-export class ShoppingListController {}
+export class ShoppingListController {
+  constructor(private readonly shoppingListService: ShoppingListService) {}
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async addProduct(@Body() { products }: AddShoppingListProductDto, @User() user: UserEntity) {
+    return this.shoppingListService.addProduct(user, products);
+  }
+}
