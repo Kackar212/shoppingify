a92c7e9843071605c5e7e2f0e9b0fdbec3c84e4c
QoB <kackar212@gmail.com>
2022-12-21 23:00:22 +0100

    Create ShoppingListProduct entity and refactor ShoppingList entity

---
+++ src/shopping-list/shopping-list-product.entity.ts
@@ -0,0 +1,19 @@
+import { Product } from 'src/products/product.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { ShoppingList } from './shopping-list.entity';
+
+@Entity()
+@Unique(['product', 'shoppingList'])
+export class ShoppingListProduct {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ default: 1 })
+  quantity: number;
+
+  @ManyToOne(() => ShoppingList, (shoppingList) => shoppingList.products)
+  shoppingList: ShoppingList;
+
+  @ManyToOne(() => Product, (product) => product.listItems)
+  product: Product;
+}
--- src/shopping-list/shopping-list.entity.ts
+++ src/shopping-list/shopping-list.entity.ts
@@ -1,22 +1,23 @@
-import { Product } from 'src/products/product.entity';
 import { User } from 'src/user/user.entity';
-import { Column, Entity, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { ShoppingListProduct } from './shopping-list-product.entity';
 
 @Entity()
 export class ShoppingList {
   @PrimaryGeneratedColumn()
   id?: number;
 
-  @Column()
-  name: string;
+  @Column({ nullable: true })
+  name?: string;
 
   @Column()
   status: string;
 
-  @ManyToMany(() => Product, { cascade: true })
-  @JoinTable()
-  products: Product[];
+  @OneToMany(() => ShoppingListProduct, (shoppingListProduct) => shoppingListProduct.shoppingList, {
+    cascade: true,
+  })
+  products: ShoppingListProduct[];
 
-  @ManyToOne(() => User, (user) => user.shoppingLists, { cascade: true })
+  @ManyToOne(() => User, (user) => user.shoppingLists)
   user: User;
 }
