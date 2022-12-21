9a7fbfa8ced38a473828a7304b89d3f1d6719bcc
QoB <kackar212@gmail.com>
2022-12-22 00:37:04 +0100

    Create ShoppingListController

--- src/shopping-list/shopping-list.module.ts
+++ src/shopping-list/shopping-list.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShoppingListProduct } from './shopping-list-product.entity';
+import { ShoppingListController } from './shopping-list.controller';
 import { ShoppingList } from './shopping-list.entity';
 import { ShoppingListService } from './shopping-list.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ShoppingList, ShoppingListProduct])],
   providers: [ShoppingListService],
+  controllers: [ShoppingListController],
 })
 export class ShoppingListModule {}
