27d18387aa356938b962244a6652257198c847e2
QoB <kackar212@gmail.com>
2022-12-22 02:17:09 +0100

    Create removeProductFromList method in ShoppingListController

--- src/shopping-list/shopping-list.controller.ts
+++ src/shopping-list/shopping-list.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Post, UseGuards, Delete } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User } from 'src/common/decorators/user.decorator';
 import { AddShoppingListProductDto } from './dto/add-shopping-list-product.dto';
 import { ShoppingListService } from './shopping-list.service';
 import { User as UserEntity } from 'src/user/user.entity';
+import { ShoppingListProductDto } from './dto/shopping-list-product.dto';
+import { RemoveListProductDto } from './dto/remove-list-product.dto';
 
 @Controller('shopping-list')
 export class ShoppingListController {
@@ -13,5 +15,14 @@ export class ShoppingListController {
   @UseGuards(JwtAuthGuard)
   async addProduct(@Body() { products }: AddShoppingListProductDto, @User() user: UserEntity) {
     return this.shoppingListService.addProduct(user, products);
+  }
+
+  @Delete()
+  @UseGuards(JwtAuthGuard)
+  removeProductFromList(
+    @Body() removeListProductData: RemoveListProductDto,
+    @User() user: UserEntity,
+  ) {
+    return this.shoppingListService.removeProductFromList(removeListProductData, user);
   }
 }
