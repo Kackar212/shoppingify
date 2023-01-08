323eb11e9e3eb41aec7ada2db0e3073b7505dc37
QoB <kackar212@gmail.com>
2023-01-08 01:42:35 +0100

    Refactor PaginationQueryDto

--- src/common/dto/pagination-query.dto.ts
+++ src/common/dto/pagination-query.dto.ts
@@ -1,13 +1,16 @@
-import { IsNumber, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsInt, Max, Min } from 'class-validator';
 
 export class PaginationQueryDto {
-  @IsNumber()
+  @IsInt()
   @Min(1)
   @Max(50)
+  @Type(() => Number)
   take?: number = 50;
 
-  @IsNumber()
+  @IsInt()
   @Min(1)
+  @Type(() => Number)
   page?: number = 1;
 
   constructor(data: PaginationQueryDto) {
