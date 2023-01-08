405e897d058d63875a273ce4e933ee0b1922ad93
QoB <kackar212@gmail.com>
2023-01-08 01:46:57 +0100

    Create PaginationInterceptor

---
+++ src/common/interceptors/pagination.interceptor.ts
@@ -0,0 +1,49 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Scope } from '@nestjs/common';
+import { map } from 'rxjs/operators';
+
+function getURLWithPage(url: URL, page: number) {
+  const requestUrl = new URL(url);
+  requestUrl.searchParams.set('page', page.toString());
+
+  return requestUrl.toString();
+}
+
+@Injectable({ scope: Scope.REQUEST })
+export class PaginationInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const request = context.switchToHttp().getRequest();
+
+    return next.handle().pipe(
+      map(async (response) => {
+        const {
+          pagination: { total, page: currentPage, take },
+        } = response;
+
+        const url = new URL(request.url, process.env.APP_URL);
+
+        const page: Record<string, number> = {};
+        page.last = Math.ceil(total / take);
+        page.first = 1;
+        page.current = currentPage > page.last || currentPage < page.last ? page.last : currentPage;
+        page.next = currentPage + 1 > page.last ? currentPage : currentPage + 1;
+        page.previous = currentPage - 1 < page.first ? page.first : currentPage - 1;
+
+        return {
+          ...response,
+          pagination: {
+            total,
+            page,
+            links: {
+              current: getURLWithPage(url, page.current),
+              last: getURLWithPage(url, page.last),
+              first: getURLWithPage(url, page.first),
+              next: getURLWithPage(url, page.next),
+              previous: getURLWithPage(url, page.previous),
+            },
+            take,
+          },
+        };
+      }),
+    );
+  }
+}
