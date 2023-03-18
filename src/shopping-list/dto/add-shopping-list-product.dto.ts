c245a1ef1806d0ca00c231b41a8274956df8b8db
QoB <kackar212@gmail.com>
2023-03-18 06:18:51 +0100

    Refactor AddShoppingListProductDto to check if every element of products array is object

--- src/shopping-list/dto/add-shopping-list-product.dto.ts
+++ src/shopping-list/dto/add-shopping-list-product.dto.ts
@@ -1,9 +1,11 @@
 import { Type } from 'class-transformer';
 import { IsArray, ValidateNested } from 'class-validator';
 import { ShoppingListProductDto } from './shopping-list-product.dto';
+import { IsEachObject } from 'src/common/decorators/is-each-object.decorator';
 
 export class AddShoppingListProductDto {
   @ValidateNested({ each: true })
+  @IsEachObject()
   @Type(() => ShoppingListProductDto)
   @IsArray()
   products: ShoppingListProductDto[];
