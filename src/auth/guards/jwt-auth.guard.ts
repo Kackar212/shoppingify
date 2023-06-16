b6a09ee2c10cbc7c71df19dc2caaae6e2821f63b
QoB <kackar212@gmail.com>
2023-06-16 23:15:46 +0200

    Refactor jwt guard, now it can authorize guests if it is used alongside AllowGuests

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { User } from 'src/user/user.entity';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector) {
+    super();
+  }
+
+  handleRequest<TUser = User>(err: any, user: TUser, info: any, context: ExecutionContext) {
+    if (err || !user) {
+      const isGuestAllowed = this.reflector.get('allow-guests', context.getHandler());
+      if (isGuestAllowed) {
+        return user;
+      }
+
+      throw err || new UnauthorizedException();
+    }
+
+    return user;
+  }
+}
