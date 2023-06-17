012ba99beef3f9345d94a022456050fa4dc0cd45
QoB <kackar212@gmail.com>
2023-06-17 06:48:46 +0200

    Add ShoppingListUser entity to ShoppingListModule

--- src/shopping-list/shopping-list.module.ts
+++ src/shopping-list/shopping-list.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ShoppingListProduct } from './shopping-list-product.entity';
+import { ShoppingListUser } from './shopping-list-user.entity';
 import { ShoppingListController } from './shopping-list.controller';
 import { ShoppingList } from './shopping-list.entity';
 import { ShoppingListService } from './shopping-list.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ShoppingList, ShoppingListProduct])],
+  imports: [TypeOrmModule.forFeature([ShoppingListUser, ShoppingList, ShoppingListProduct])],
   providers: [ShoppingListService],
   controllers: [ShoppingListController],
 })
