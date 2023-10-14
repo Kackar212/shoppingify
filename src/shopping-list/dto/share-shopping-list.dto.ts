3a7577b140a002fd8bbc9a6caf19269f469b9c67
Kackar212 <kackar212@gmail.com>
2023-10-14 14:49:44 +0200

    Refactor ShareShoppingListDto

--- src/shopping-list/dto/share-shopping-list.dto.ts
+++ src/shopping-list/dto/share-shopping-list.dto.ts
@@ -1,19 +1,32 @@
 import { Type } from 'class-transformer';
 import {
+  IsAlphanumeric,
   IsArray,
   IsBoolean,
   IsEmail,
   IsEnum,
   IsInt,
   IsOptional,
+  IsString,
+  Length,
+  Matches,
+  ValidateIf,
   ValidateNested,
 } from 'class-validator';
 import { IsEachObject } from 'src/common/decorators/is-each-object.decorator';
 import { SHARED_LIST_USER_ROLE } from '../enums/shared-list-user-role.enum';
 
 class User {
+  @ValidateIf((user) => !user.name || user.email)
   @IsEmail()
   email: string;
+
+  @ValidateIf((user) => !user.email || user.name)
+  @IsString()
+  @Length(3, 20)
+  @IsAlphanumeric()
+  @Matches(/^[A-Z].*/i)
+  name: string;
 
   @IsEnum(SHARED_LIST_USER_ROLE)
   @IsOptional()
